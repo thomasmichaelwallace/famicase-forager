325acc7ad204dd4db73793445dea7a3e6548dea6
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-09-26 14:26:43 +0100

    feat: fire to interact

--- Assets/Scripts/ControlledBehaviour.cs
+++ Assets/Scripts/ControlledBehaviour.cs
@@ -3,24 +3,39 @@ using UnityEngine;
 
 public class ControlledBehaviour : MonoBehaviour
 {
+    private Camera _camera;
     private CinemachineBasicMultiChannelPerlin _cameraNoise;
     private CharacterController _characterController;
 
     private void Start()
     {
         _characterController = GetComponent<CharacterController>();
-        var camera = FindObjectOfType<CinemachineVirtualCamera>();
-        _cameraNoise = camera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        var virtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
+        _cameraNoise = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        _camera = Camera.main;
     }
 
     private void Update()
     {
         var input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
-        var angle = Camera.main.transform.eulerAngles.y;
+
+        var angle = _camera.transform.eulerAngles.y;
         var move = Quaternion.Euler(0f, angle, 0f) * input;
-
-        _cameraNoise.m_AmplitudeGain = Mathf.MoveTowards(_cameraNoise.m_AmplitudeGain, move.magnitude, Time.deltaTime * 5f);
-
         _characterController.SimpleMove(move);
+
+        _cameraNoise.m_AmplitudeGain =
+            Mathf.MoveTowards(_cameraNoise.m_AmplitudeGain, input.magnitude, Time.deltaTime * 5f);
+
+        var fire = Input.GetButton("Fire1");
+        if (fire)
+        {
+            Vector3 dir = Quaternion.Euler(_camera.transform.eulerAngles) * Vector3.forward;
+            var hit = Physics.Raycast(transform.position, dir, out var hitInfo);
+            if (hit)
+            {
+                var interactableBehaviour = hitInfo.transform.GetComponent<InteractableBehaviour>();
+                if (interactableBehaviour) interactableBehaviour.Toggle();
+            }
+        }
     }
 }
---
+++ Assets/Scripts/InteractableBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class InteractableBehaviour : MonoBehaviour
+{
+    public Material selected;
+
+    private Material _material;
+    private Renderer _renderer;
+    private bool _toggled;
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+        _material = _renderer.material;
+    }
+
+    public void Toggle()
+    {
+        _toggled = !_toggled;
+        _renderer.material = _toggled ? selected : _material;
+    }
+}
