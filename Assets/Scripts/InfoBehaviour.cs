33e25f719b38a0b8acd4f16327d6bcd4cc7c5d54
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-09-26 16:53:44 +0100

    feat: readable popup

---
+++ Assets/Scripts/InfoBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InfoBehaviour : MonoBehaviour
+{
+    private CanvasGroup _canvas;
+    private bool _shown;
+
+    private void Start()
+    {
+        _canvas = GetComponent<CanvasGroup>();
+    }
+
+    public void Toggle(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        _shown = !_shown;
+        _canvas.alpha = _shown ? 1 : 0;
+    }
+}
