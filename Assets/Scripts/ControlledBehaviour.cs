ae5029e3d5cbe7b9645f08476f2d001bdcd6f04d
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-09-26 11:36:08 +0100

    feat: simple fps movement and control

---
+++ Assets/Scripts/ControlledBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlledBehaviour : MonoBehaviour
+{
+    private CharacterController _characterController;
+    
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
+        float angle = Camera.main.transform.eulerAngles.y;
+        Vector3 move = Quaternion.Euler(0f, angle, 0f) * input;
+        
+        _characterController.SimpleMove( move);
+    }
+}
