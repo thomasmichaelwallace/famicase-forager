2df8c7a3d80e0162d7c65923ade6c8c53851829a
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-09-27 11:17:10 +0100

    feat: end game

---
+++ Assets/Scripts/MushFinal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MushFinal : MonoBehaviour, IInteractable
+{
+    public CanvasGroup screen;
+
+    private TextMeshProUGUI _text;
+    
+    private bool _collected;
+
+    private void Start()
+    {
+        _text = screen.GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        if (!_collected) return;
+        if (screen.alpha < 1)
+        {
+            screen.alpha += Time.deltaTime;
+        }
+    }
+
+    public void Interact(ControlledBehaviour controlled)
+    {
+        _collected = true;
+        _text.text = "You Win!";
+    }
+}
