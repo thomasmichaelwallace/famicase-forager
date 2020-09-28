1a2825d5e54ac9f16cbcf67c30ff0401a87c9235
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-09-28 15:05:47 +0100

    fix: lock mouse for better control

---
+++ Assets/Scripts/MouseControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Grab()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    } 
+}
