400bb247ec59dfcd30f977a485b674ed6e3b3fc3
thomas michael wallace <thomasmichaelwallace@gmail.com>
2020-10-10 10:46:15 +0100

    feat: quality settings

---
+++ Assets/Scripts/Quality.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.PostProcessing;
+
+public class Quality : MonoBehaviour
+{
+    private float autoSwitchRenderTime = 500f;
+    private PostProcessLayer _post;
+
+    void Start()
+    {
+        _post = Camera.main.GetComponent<PostProcessLayer>();
+    }
+    
+    void Update()
+    {
+        if (Time.deltaTime > autoSwitchRenderTime)
+        {
+            _post.enabled = false;
+        }
+    }
+    
+    public void Toggle(InputAction.CallbackContext context)
+    {
+        if (context.performed) _post.enabled = !_post.enabled;
+    }
+}
